02a1196b26439823a25d223dda04e97eb0a9324a
vaughnouano <vaughneurielouano@gmail.com>
2026-03-06 01:12:56 +0800

    reappended items to parent

--- Item.java
+++ Item.java
@@ -11,3 +11,29 @@ class Item {
         System.out.println("Using  " + name + ": " + effect);
     }
 }
+
+class Weapon extends Item {
+   private int bonusDamage;
+
+   public Weapon(String var1, int var2) {
+      super(var1, "Deals extra damage");
+      this.bonusDamage = var2;
+   }
+
+   public void use() {
+      System.out.println("Equipped " + this.name + "! Attack +" + this.bonusDamage);
+   }
+}
+
+class Potion extends Item {
+   private int healAmount;
+
+   public Potion(String var1, int var2) {
+      super(var1, "Restores HP");
+      this.healAmount = var2;
+   }
+
+   public void use() {
+      System.out.println("Drank " + this.name + "! Healed " + this.healAmount + " HP");
+   }
+}
